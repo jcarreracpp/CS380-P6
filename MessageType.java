dd942c9a84187b8b391799f0057dbd9c33f3335a
jcarreracpp <jcarrera@cpp.edu>
2017-05-26 22:43:44 -0700

    Works as intended.

--- MessageType.java
+++
@@ -1,6 +0,0 @@
-public enum MessageType {
-
-    CONNECT, COMMAND, BOARD, MOVE, ERROR;
-
-    private static final long serialVersionUID = 0L;
-}
--- TicTacToeClient.java
+++ TicTacToeClient.java
@@ -8,6 +8,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 
 /**
@@ -18,35 +19,78 @@ public class TicTacToeClient {
 
     public static void main(String[] args) throws Exception {
         try (Socket socket = new Socket("codebank.xyz", 38006)) {
-            InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-            BufferedReader br = new BufferedReader(isr);
-            OutputStream os = socket.getOutputStream();
-            ByteArrayOutputStream b = new ByteArrayOutputStream();
-            byte[] marps = new byte[10];
-            ByteArrayInputStream bin = new ByteArrayInputStream(marps);
-            ObjectOutputStream o = new ObjectOutputStream(b);
-            ObjectInputStream oi = new ObjectInputStream(bin);
+            ObjectOutputStream oo = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream oi = new ObjectInputStream(socket.getInputStream());
+            Scanner in = new Scanner(System.in);
+            
             ConnectMessage oup = new ConnectMessage("Jorge");
             CommandMessage begin = new CommandMessage(CommandMessage.Command.NEW_GAME);
+            MoveMessage move;
             
-            o.writeObject(oup);
-            o.writeObject(begin);
-            System.out.println("HW");
+            oo.writeObject(oup);
+            oo.writeObject(begin);
             
-            oi.read();
-            int[][] map = new int[3][3];
-            for(int i = 0 ; i < 3 ; i++){
-                for(int j = 0; j < 3; j++){
-                map[i][j] = is.read();
+            BoardMessage response = (BoardMessage)oi.readObject();
+            
+            byte row;
+            byte col;
+            boolean errorAvoided = true;
+            
+            while (response.getStatus() == BoardMessage.Status.IN_PROGRESS && errorAvoided) {
+
+                byte[][] map = response.getBoard();
+                char[][] c = convertTTT(map);
+
+                for (int n = 0; n < 3; n++) {
+                    for (int p = 0; p < 3; p++) {
+                        System.out.print(c[n][p] + " ");
+                    }
+                    System.out.println();
+                }
+                System.out.print("\nEnter row (1-3): ");
+                row = Byte.valueOf(in.nextLine());
+                row--;
+                System.out.print("\nEnter column (1-3): ");
+                col = Byte.valueOf(in.nextLine());
+                col--;
+                System.out.println();
+                
+                move = new MoveMessage(row, col);
+                oo.writeObject(move);
+                try{
+                    response = (BoardMessage)oi.readObject();
+                } catch(ClassCastException e){
+                    System.out.println("Error encountered! Exiting game.");
+                    begin = new CommandMessage(CommandMessage.Command.EXIT);
+                    oo.writeObject(begin);
+                    errorAvoided = false;
                 }
             }
-            for(int k =0 ; k < 3; k++){
-                for(int m = 0; m < 3; m++){
-                    System.out.print(map[k][m] + " ");
-                }
-                System.out.println();
+            if(!errorAvoided){
+            }else if(response.getStatus() == BoardMessage.Status.PLAYER1_VICTORY){
+                System.out.println("You Won!");
+            }else if(response.getStatus() == BoardMessage.Status.PLAYER2_VICTORY){
+                System.out.println("You Lost!");
+            }else if(response.getStatus() == BoardMessage.Status.STALEMATE){
+                System.out.println("Tie!");
             }
         }
+    }
+    public static char[][] convertTTT(byte[][] b){
+        char[][] c = new char[3][3];
+        
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j <3; j++){
+                switch(b[i][j]){
+                    case 0: c[i][j] = '-';
+                            break;
+                    case 1: c[i][j] = 'X';
+                            break;
+                    default:c[i][j] = 'O';
+                            break;
+                }
+            }
+        }
+        return c;
     }
 }
